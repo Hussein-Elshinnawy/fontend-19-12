12ddaff0bd0deeb6f5a2727adccc7c4cc071474b
Hussein <94644643+Hussein-Elshinnawy@users.noreply.github.com>
2022-12-20 22:59:41 +0200

    Update trip.service.ts

--- src/app/trip.service.ts
+++ src/app/trip.service.ts
@@ -11,26 +11,26 @@ export class TripService {
 
   create(tripData:any):Observable<any>
   {
-    return this._HttpClient.post('https://conback-git-milcu18076-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/admin/create',tripData)
+    return this._HttpClient.post('https://back100-milcu18076-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/admin/create',tripData)
 
   }
 
   getAll():Observable<any>
   {
-    return this._HttpClient.get('https://conback-git-milcu18076-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/admin/getAllTrips')
+    return this._HttpClient.get('https://back100-milcu18076-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/admin/getAllTrips')
 
   }
 
 
   updateTrip(tripData:any, id:number):Observable<any>
   {
-    return this._HttpClient.put('https://conback-git-milcu18076-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/admin/updateTrip/'+id,tripData)
+    return this._HttpClient.put('https://back100-milcu18076-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/admin/updateTrip/'+id,tripData)
 
   }
 
   deleteTrip(id:number):Observable<any>
   {
-    return this._HttpClient.delete('https://conback-git-milcu18076-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/admin/deleteTrip/'+id)
+    return this._HttpClient.delete('https://back100-milcu18076-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/admin/deleteTrip/'+id)
 
   }
 
